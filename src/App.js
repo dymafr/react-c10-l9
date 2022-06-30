3bd77525cf1f442345b6d7247c0dc54a2dbbc060
dymafr <erwan1309@hotmail.com>
2022-06-30 18:23:25 +0200

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { useState, memo } from 'react';
-import Calendar from './Calendar';
+import React, { useState, useMemo } from 'react';
 
 export default function App() {
   console.log('Rendu App');
-  const [value, setValue] = useState('');
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+  const [list, setList] = useState([]);
+
+  function complexCalculation(list) {
+    console.log('Invocation');
+    return list.map((l) => {
+      for (let i = 0; i < 1000000; i++) {
+        l = i;
+      }
+      return l;
+    });
+  }
+
+  const complexList = useMemo(() => complexCalculation(list), [list]);
 
   return (
     <>
       <input
         className="m-10"
-        type="text"
+        type="number"
         onChange={(e) => setValue(e.target.value)}
         value={value}
       />
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <Calendar count={count} />
+      <button
+        onClick={() => {
+          setList([...list, value]);
+          setValue(0);
+        }}
+      >
+        Ajouter
+      </button>
+      <ul>
+        {complexList.map((cl, i) => (
+          <li key={i}>{cl}</li>
+        ))}
+      </ul>
     </>
   );
 }
